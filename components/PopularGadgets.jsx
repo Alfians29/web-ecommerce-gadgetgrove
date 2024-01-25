2eab0bd5ea15372000709fec36cbddd987f0862c
Muhammad Alfian <me.alfians29@gmail.com>
2024-01-25 15:17:53 +0700

    Update v.3.0
    - add product detail page
    - add product slug
    - update data fetching
    - update accent color

--- components/PopularGadgets.jsx
+++ components/PopularGadgets.jsx
@@ -5,17 +5,17 @@ import PopularGadgetsCarousel from './PopularGadgetsCarousel';
 // Get data
 const getData = async () => {
   const query = `*[_type == 'product' && references(*[_type == 'category' && name == 'popular']._id, categories)] {
-        _id,
-        name,
-        description,
-        images,
-        price,
-        price_id,
-        "slug": slug.current,
-        "categories": categories[] -> {
-        name
-        }
-    }`;
+    _id,
+      name,
+      description,
+      images,
+      price,
+      price_id,
+      "slug": slug.current,
+      "categories": categories[] -> {
+      name
+      }
+  }`;
   const data = await client.fetch(query);
   return data;
 };
