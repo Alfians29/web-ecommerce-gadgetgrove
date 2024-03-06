28ed4f4a9d7cbd2483b40e30eb237d73cb76f051
Muhammad Alfian <me.alfians29@gmail.com>
2024-03-06 14:44:13 +0700

    Update v.7.0
    - add page.jsx (our-gadgets)
    - add Hero.jsx  (components)
    - update app page.jsx
    - update components (Header.jsx & Nav,jsx)

---
+++ app/our-gadgets/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OurGadgets = () => {
+  return <div>Our Gadgets</div>;
+};
+
+export default OurGadgets;
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
+import Hero from '@/components/Hero';
 import PopularGadgets from '@/components/PopularGadgets';
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <PopularGadgets />
     </main>
   );
